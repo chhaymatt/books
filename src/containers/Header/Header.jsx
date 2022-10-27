6f2ae0f4fb8f062abe32649d0946e2e336ce319b
Matthew Chhay <chhaymatt@gmail.com>
2022-10-27 22:04:14 +1100

    Updated README

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -3,7 +3,7 @@ import styles from "./Header.module.scss"
 const Header = () => {
     return (
         <div className={styles.Header}>
-            <h1 className={styles.Header__Heading}>Books</h1>
+            <h1 className={styles.Header__Heading}>Book Search</h1>
             <p className={styles.Header__Text}>Discover your next favourite book</p>
         </div>
     )
