4476a4cc1d77ab6905e5f98e1c9f02b3139b52c7
Matthew Chhay <chhaymatt@gmail.com>
2022-10-30 15:13:40 +1100

    Improved responsiveness for phones

--- src/components/ToggleBar/ToggleBar.jsx
+++ src/components/ToggleBar/ToggleBar.jsx
@@ -3,7 +3,7 @@ import styles from "./ToggleBar.module.scss"
 const ToggleBar = ({name, nameDisplay, options, onInputChange}) => {
     return (
         <div className={styles.ToggleBar}>
-            <div>{nameDisplay}</div>
+            <div className={styles.ToggleBar__Name}>{nameDisplay}</div>
             <select className={styles.ToggleBar__Dropdown} onChange={onInputChange} name={name} >
                 {options.map((element, index) => (<option key={index} value={element}>{element}</option>))}
             </select>
